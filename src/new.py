c75d40e32b444faa2d5508f1f7b834e9b237af00
SfyXboh <rexswang17@gmail.com>
2024-04-04 12:26:26 -0700

    HSV based colour detection with a calibration function

---
+++ src/new.py
@@ -0,0 +1,160 @@
+# Python code for Multiple Color Detection 
+  
+  
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import exposure
+from skimage.exposure import match_histograms
+import cv2 
+from util2 import calibrate
+from util2 import colour_limits
+  
+r, g, b = calibrate()
+h_tol = 5
+s_tol = 10
+v_tol = 20
+
+# Capturing video through webcam 
+webcam = cv2.VideoCapture(0) 
+  
+# Start a while loop 
+while(1): 
+      
+    # Reading the video from the 
+    # webcam in image frames 
+    _, imageFrame = webcam.read() 
+
+    # locate the screen
+    height, width, _ = imageFrame.shape
+    screen_w = int(width/4)
+    screen_h = int(screen_w/4*5.2)
+    screen_x = int(width/2-screen_w/2)
+    screen_y = int(height/2-screen_h/1.5)
+
+    # locate first button
+    b1_w = int(screen_w/4*0.5)
+    b1_h = b1_w
+    b1_x = int(screen_x - 1.5*b1_w)
+    b1_y = int(screen_y + screen_h/5.2*0.55)
+    b2_w = b1_w
+    b2_h = b1_w
+    b2_x = b1_x
+    b2_y = b1_y + 2*b1_h
+
+    cv2.rectangle(imageFrame, (screen_x, screen_y), (screen_x + screen_w, screen_y + screen_h), (0, 255, 0), 2)
+    cv2.rectangle(imageFrame, (b1_x, b1_y), (b1_x + b1_w, b1_y + b1_h), (0, 255, 0), 2)
+    cv2.rectangle(imageFrame, (b2_x, b2_y), (b2_x + b2_w, b2_y + b2_h), (0, 255, 0), 2)
+  
+    # Convert the imageFrame in  
+    # BGR(RGB color space) to  
+    # HSV(hue-saturation-value) 
+    # color space 
+    hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
+  
+    # Set range for red color and  
+    # define mask 
+    red_lower, red_upper = colour_limits(r, h_tol, s_tol, v_tol) 
+    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
+  
+    # # Set range for green color and  
+    # # define mask 
+    # green_lower, green_upper = colour_limits(g, h_tol, s_tol, v_tol)
+    # green_mask = cv2.inRange(hsvFrame, green_lower, green_upper) 
+  
+    # # Set range for blue color and 
+    # # define mask 
+    # blue_lower, blue_upper = colour_limits(b, h_tol, s_tol, v_tol) 
+    # blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
+      
+    # Morphological Transform, Dilation 
+    # for each color and bitwise_and operator 
+    # between imageFrame and mask determines 
+    # to detect only that particular color 
+    kernel = np.ones((5, 5), "uint8") 
+      
+    # For red color 
+    red_mask = cv2.dilate(red_mask, kernel) 
+    res_red = cv2.bitwise_and(imageFrame, imageFrame,  
+                              mask = red_mask) 
+      
+    # For green color 
+    # green_mask = cv2.dilate(green_mask, kernel) 
+    # res_green = cv2.bitwise_and(imageFrame, imageFrame, 
+    #                             mask = green_mask) 
+      
+    # # For blue color 
+    # blue_mask = cv2.dilate(blue_mask, kernel) 
+    # res_blue = cv2.bitwise_and(imageFrame, imageFrame, 
+    #                            mask = blue_mask) 
+   
+    # Creating contour to track red color 
+    contours, hierarchy = cv2.findContours(red_mask, 
+                                           cv2.RETR_TREE, 
+                                           cv2.CHAIN_APPROX_SIMPLE) 
+
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            cx = int(x + w/2)
+            cy = int(y + h/2)
+            
+            # imageFrame = cv2.rectangle(imageFrame, (x, y),  
+            #                            (x + w, y + h),  
+            #                            (0, 0, 255), 2) 
+              
+            cv2.putText(imageFrame, "Red Colour", (x, y), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+                        (0, 0, 255))   
+            if cx >= b1_x and cx <= b1_x + b1_w and cy >= b1_y and cy <= b1_y + b1_h:
+                cv2.rectangle(imageFrame, (b1_x, b1_y), (b1_x + b1_w, b1_y + b1_h), (0, 255, 0), -1) 
+            if cx >= b2_x and cx <= b2_x + b2_w and cy >= b2_y and cy <= b2_y + b2_h:
+                cv2.rectangle(imageFrame, (b2_x, b2_y), (b2_x + b2_w, b2_y + b2_h), (0, 0, 255), -1) 
+            
+            cv2.circle(imageFrame, (cx, cy), 3, (255, 0, 0), 3) # center point of red area
+  
+    # # Creating contour to track green color 
+    # contours, hierarchy = cv2.findContours(green_mask, 
+    #                                        cv2.RETR_TREE, 
+    #                                        cv2.CHAIN_APPROX_SIMPLE) 
+      
+    # for pic, contour in enumerate(contours): 
+    #     area = cv2.contourArea(contour) 
+    #     if(area > 300): 
+    #         x, y, w, h = cv2.boundingRect(contour) 
+    #         imageFrame = cv2.rectangle(imageFrame, (x, y),  
+    #                                    (x + w, y + h), 
+    #                                    (0, 255, 0), 2) 
+              
+    #         cv2.putText(imageFrame, "Green Colour", (x, y), 
+    #                     cv2.FONT_HERSHEY_SIMPLEX,  
+    #                     1.0, (0, 255, 0)) 
+  
+    # # Creating contour to track blue color 
+    # contours, hierarchy = cv2.findContours(blue_mask, 
+    #                                        cv2.RETR_TREE, 
+    #                                        cv2.CHAIN_APPROX_SIMPLE) 
+    # for pic, contour in enumerate(contours): 
+    #     area = cv2.contourArea(contour) 
+    #     if(area > 300): 
+    #         x, y, w, h = cv2.boundingRect(contour) 
+    #         imageFrame = cv2.rectangle(imageFrame, (x, y), 
+    #                                    (x + w, y + h), 
+    #                                    (255, 0, 0), 2) 
+              
+    #         cv2.putText(imageFrame, "Blue Colour", (x, y), 
+    #                     cv2.FONT_HERSHEY_SIMPLEX, 
+    #                     1.0, (255, 0, 0)) 
+
+
+    # calibrate again
+    if cv2.waitKey(1) == 99: # press c
+        r,g,b = calibrate() 
+       
+    # Program Termination 
+    cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame) 
+    if cv2.waitKey(10) & 0xFF == ord('q'): 
+        webcam.release() 
+        cv2.destroyAllWindows() 
+        break
---
+++ src/util2.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import exposure
+from skimage.exposure import match_histograms
+import cv2
+
+def colour_limits(c, h_tol, s_tol, v_tol):
+    # converts bgr values to hsv colour space
+    # commonly used for object tracking
+
+    # not sure what this does...
+    lowLimit = np.array([max(0,c[0]-h_tol), max(0,c[1]-s_tol), max(0,c[2]-v_tol)], np.uint8)
+    upLimit = np.array([min(179,c[0]+h_tol), min(255,c[1]+s_tol), min(255,c[2]+v_tol)], np.uint8)
+
+    return lowLimit, upLimit
+
+def calibrate():
+    webcam = cv2.VideoCapture(0) 
+  
+    # Start a while loop 
+    while(1): 
+        _, frame = webcam.read()
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        height, width, _ = frame.shape
+
+        # locate centre
+        cx = int(width / 2)
+        cy = int(height / 2)
+        cv2.circle(frame, (cx, cy),  5, (255, 0, 0), 3)
+        
+        cv2.imshow("calibration", frame)
+        key = cv2.waitKey(1)
+        if key == 114: # pressed r
+            red = hsv_frame[cy, cx]
+            print(red)
+        if key == 103: # pressed g
+            green = hsv_frame[cy, cx]
+            print(green)
+        if key == 98: # pressed b
+            blue = hsv_frame[cy, cx]
+            print(blue)
+        if key == 32: # pressed space
+            webcam.release() 
+            cv2.destroyAllWindows() 
+            break
+    return red, green, blue
+
+        
